7a676e4aebaf269e4ce3313c6a3b01d063bf4d03
dytroInc <dytrocodesworkshop@gmail.com>
2022-02-17 00:11:36 +0900

    Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,8 @@ export default function App({ Component, pageProps }) {
             <meta name="description" content="각니메들을 손쉽게 플레이리스트로 스트리밍하세요." />
             <meta name="keywords" content="각별, 각니메, 각프텔" />
             <meta name="theme-color" content="#eeee00" />
+
+            <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         {router.route !== '/item/[item]/[episode]' && <Header />}
         <Component style={{
